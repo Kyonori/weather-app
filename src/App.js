456a713279180366499d3a6661f76babe89e1c37
kyonori1 <reustache2019@bklawtech.com>
2018-02-12 23:12:56 +0000

    second  weather commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom'
+import DayPage from './DayPage'
+import WeekPage from './WeekPage'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <header>
+              <img src={logo} alt = 'React Logo'> </img>
+          </header>
+        </div>
+        
+/*      <Router>
+       <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/news">NewsFeed</Link>
+        </li>a
+      </ul>
+
+      <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/news" component={NewsFeed} />
+    </div>
+      </Router>
+    );
+  }
+} */
+
+export default App;
+
+/* <div className="App">
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
-  }
-}
-
-export default App;
+*/
